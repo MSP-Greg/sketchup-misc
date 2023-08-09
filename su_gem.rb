454a7b574ce5dfd3be8c795380bda1cb7edeb382
MSP-Greg <Greg.mpls@gmail.com>
2023-08-08 21:43:06 -0500

    su_gem.rb - Add SUGem.su_list

--- su_gem.rb
+++ su_gem.rb
@@ -22,91 +22,180 @@
 #
 #     SUGem.run "install hike --user-install -N"
 #
+# An additional command is available, which lists all gems available to SketchUp,
+# and also shows their location/type
+# The command is `SUGem.su_list`
+
 module SUGem
+
+  GEM_PLATFORMS = Gem.platforms.reject { |p| p == 'ruby' }.map(&:to_s)
 
   class << self
 
-  def run args
-    returned = false
+    def run args
+      returned = false
 
-    # load here so only loaded when run
-    require 'stringio'
-    require 'rubygems'
-    require 'rubygems/command_manager'
-    require 'rubygems/config_file'
-    require 'rubygems/deprecate'
+      # load here so only loaded when run
+      require 'stringio'
+      require 'rubygems'
+      require 'rubygems/command_manager'
+      require 'rubygems/config_file'
+      require 'rubygems/deprecate'
 
-    ary_args = args.split(/ +/)
+      ary_args = args.split(/ +/)
 
-    cmd = Gem::CommandManager.instance
-    # fix abbreviation
-    ary_args[0] = 'environment' if ary_args[0] == 'env'
-    unless cmd.command_names.include? ary_args[0]
-      puts "SUGem - #{ary_args[0]} is not a valid gem command!"
-      returned = true
-      return
+      cmd = Gem::CommandManager.instance
+      # fix abbreviation
+      ary_args[0] = 'environment' if ary_args[0] == 'env'
+
+      unless cmd.command_names.include? ary_args[0]
+        puts "SUGem - #{ary_args[0]} is not a valid gem command!"
+        returned = true
+        return
+      end
+
+      build_args = extract_build_args ary_args
+
+      do_configuration ary_args
+
+      cmd.command_names.each do |command_name|
+        config_args = Gem.configuration[command_name]
+        config_args = case config_args
+                      when String
+                        config_args.split ' '
+                      else
+                        Array(config_args)
+                      end
+        Gem::Command.add_specific_extra_args command_name, config_args
+      end
+
+      sio_in = StringIO.new
+      sio_out, sio_err = StringIO.new, StringIO.new
+      cmd.ui = Gem::StreamUI.new(sio_in, sio_out, sio_err, false)
+
+      cmd.run Gem.configuration.args, build_args
+      t = sio_err.string
+      puts "err #{t}\n" unless t.empty?
+    rescue Gem::SystemExitException => e
+      t = e.message
+      puts t unless t.end_with? "exit_code 0"
+      t = sio_err.string
+      puts t unless t.empty?
+    ensure
+      return if returned
+      t = sio_out ? sio_out.string : ''
+      puts t unless t.empty?
+      sio_in  and sio_in.close
+      sio_out and sio_out.close
+      sio_err and sio_err.close
     end
 
-    build_args = extract_build_args ary_args
+    def su_list
+      dash  = 8212.chr(Encoding::UTF_8)
+      width = 75
+      dash_line = dash * width
 
-    do_configuration ary_args
+      # if a gem exists in multiple locations, @names[name] will be > 1
+      names = Hash.new { |h,k| h[k] = 0 }
+      dflt_spec_dir = Gem.respond_to?(:default_specifications_dir) ?
+        Gem.default_specifications_dir : Gem::BasicSpecification.default_specifications_dir
+      dflt      = extract names, dflt_spec_dir
+      bundled   = extract names, File.join(Gem.default_dir, 'specifications')
+      installed = extract names, File.join(Gem.dir        , 'specifications')
+      user      = extract names, File.join(Gem.user_dir   , 'specifications')
 
-    cmd.command_names.each do |command_name|
-      config_args = Gem.configuration[command_name]
-      config_args = case config_args
-                    when String
-                      config_args.split ' '
-                    else
-                      Array(config_args)
-                    end
-      Gem::Command.add_specific_extra_args command_name, config_args
+      str =  "\n#{dash_line} Installed Gems\n".dup
+      str << "Bundled   #{Gem.default_dir}\n"
+      str << "Installed #{Gem.dir}\n"
+      str << "User      #{Gem.user_dir}\n"
+      str << "* gem exists in multiple locations\n\n"
+
+      str << "#{dash * 12} Default Gems #{dash * 12}\n"
+      str << output(names, dflt, "D ")
+
+      str << "#{dash * 12} Bundled Gems #{dash * 12} \n"
+      str << output(names, bundled, "B ")
+
+      str << "#{dash * 12} Installed Gems #{dash * 10} \n"
+      str << output(names, installed, "I ")
+
+      str << "#{dash * 12} User Gems #{dash    * 15} \n"
+      str << output(names, user, "U ")
+      puts str.gsub(/#{ENV['USER']}/, '<user>')
     end
 
-    sio_in = StringIO.new
-    sio_out, sio_err = StringIO.new, StringIO.new
-    cmd.ui = Gem::StreamUI.new(sio_in, sio_out, sio_err, false)
+    private
 
-    cmd.run Gem.configuration.args, build_args
-    t = sio_err.string
-    puts "err #{t}\n" unless t.empty?
-  rescue Gem::SystemExitException => e
-    t = e.message
-    puts t unless t.end_with? "exit_code 0"
-    t = sio_err.string
-    puts t unless t.empty?
-  ensure
-    return if returned
-    t = sio_out ? sio_out.string : ''
-    puts t unless t.empty?
-    sio_in  and sio_in.close
-    sio_out and sio_out.close
-    sio_err and sio_err.close
-  end
+    ##
+    # Separates the build arguments (those following <code>--</code>) from the
+    # other arguments in the list.
 
-  private
+    def extract_build_args args # :nodoc:
+      return [] unless offset = args.index('--')
+      build_args = args.slice!(offset...args.length)
+      build_args.shift
+      build_args
+    end
 
-  ##
-  # Separates the build arguments (those following <code>--</code>) from the
-  # other arguments in the list.
+    def do_configuration(args)
+      Gem.configuration = Gem::ConfigFile.new(args)
+      Gem.use_paths Gem.configuration[:gemhome], Gem.configuration[:gempath]
+      Gem::Command.extra_args = Gem.configuration[:gem]
+    end
 
-  def extract_build_args args # :nodoc:
-    return [] unless offset = args.index('--')
-    build_args = args.slice!(offset...args.length)
-    build_args.shift
-    build_args
-  end
+    # used by gem_list
+    def output(names, ary, pre)
+      cntr = 1
+      str = ''.dup
+      ary.each do |a|
+        if names[a[0]] > 1
+          str << "#{pre} #{a[0].ljust 25} * #{a[1]}\n"
+        else
+          str << "#{pre} #{a[0].ljust 25}   #{a[1]}\n"
+        end
+        str << "\n" if (cntr % 5) == 0
+        cntr += 1
+      end
+      # str can contain two returns at end
+      str.rstrip + "\n\n"
+    end
 
-  def do_configuration(args)
-    Gem.configuration = Gem::ConfigFile.new(args)
-    Gem.use_paths Gem.configuration[:gemhome], Gem.configuration[:gempath]
-    Gem::Command.extra_args = Gem.configuration[:gem]
-  end
+    # used by gem_list
+    def extract(names, spec_dir)
+      gem_ary = Dir['*.gemspec', base: spec_dir]
+      ary = []
+      gem_ary.each do |fn|
+        full = fn.sub(/\.gemspec\z/, '').dup
+        platform = nil
+        GEM_PLATFORMS.each do |p|
+          if full.end_with? p
+            platform = p
+            full.sub! "-#{p}", ''
+            break
+          end
+        end
 
-  def method_missing(meth, arg = '')
-    raise ArgumentError('SUGem - argument must be a string') unless String === arg
-    arg =  "#{meth} #{arg}"
-    run arg
-  end
+        name, _, vers = full.rpartition '-'
 
+        ary << [name, Gem::Version.new(vers), platform]
+      end
+
+      hsh = ary.group_by(&:first)
+      ary = []
+      hsh.each do |k,v|
+        val = v.sort { |a,b| b[1] <=> a[1] }
+          .map { |i| i[2].nil? ? i[1] : "#{i[1]}-#{i[2]}" }
+          .join ' '
+        ary << [k, val]
+        names[k] += 1
+      end
+      ary.sort
+    end
+
+    def method_missing(meth, arg = '')
+      raise ArgumentError('SUGem - argument must be a string') unless String === arg
+      arg =  "#{meth} #{arg}"
+      run arg
+    end
   end # class << self
 end
